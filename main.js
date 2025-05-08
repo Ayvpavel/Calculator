24772c1bfb7cdc276b1e09bcf03a1afbe819dc1b
unknown <pavel.1504@mail.ru>
2025-05-09 00:48:43 +1000

    Реоизовал функции калькулятора

--- main.js
+++ main.js
@@ -1,83 +1,84 @@
-const resultElement = document.getElementById(`result`)
-const input = document.getElementById(`input`)
-const input2 = document.getElementById(`input2`)
-const submitBtn = document.getElementById(`submit`)
-const plusBtn = document.getElementById(`plus`)
-const minusBtn = document.getElementById(`minus`)
-const divisionBtn = document.getElementById(`division`)
-const multiplicationBtn = document.getElementById(`multiplication`)
-const numbers = []
-let submitcount = 0
+const resultElement = document.getElementById(`result`);
+const input = document.getElementById(`input`);
+const input2 = document.getElementById(`input2`);
+const submitBtn = document.getElementById(`submit`);
+const plusBtn = document.getElementById(`plus`);
+const minusBtn = document.getElementById(`minus`);
+const divisionBtn = document.getElementById(`division`);
+const multiplicationBtn = document.getElementById(`multiplication`);
+let sum = 0;
+let action = ' ';
+let countSubmit = 0;
 
-let action = ``
-let sum = 0
-let number2 = 0
+plusBtn.onclick = function () {
+  sum = 0;
+  action = '+';
+  if (input.value !== '') {
+    sum = Number(input.value) + sum;
+  }
+  input.value = '';
+  countSubmit = 0;
+};
 
-// input1.addEventListener()
+submitBtn.onclick = function () {
+  if (countSubmit == 1) {
+    return;
+  }
+  countSubmit = countSubmit + 1;
 
-function even(name,plus) 
-{
-    console.log(name,plus)
-} 
-// even("123",143)
-// let name = "123"
-// let plus = "143"
+  if (action == '-') {
+    sum = sum - Number(input.value);
+  }
+  if (action == '+') {
+    sum = Number(input.value) + sum;
+  }
 
+  if (action == '*') {
+    sum = Number(input.value) * sum;
+  }
+  if (action == '/') {
+    sum = sum / Number(input.value);
+  }
+  
+  input.value = sum;
 
-// console.log(input1.value)
-// console.log(resultElement.textContent)
-// resultElement.textContent = 88
-// // console.log(typeof sum)
+  if (action == '+') {
+    sum = 0;
+  }
+  if (action == '*') {
+    sum = 1;
+  }
+  if (action == '/') {
+    sum = 1;
+  }
+};
 
+minusBtn.onclick = function () {
+  sum = 0;
+  if (input.value !== '') {
+    sum = Number(input.value) - sum;
+  }
+  input.value = '';
+  action = '-';
+  countSubmit = 0;
+};
 
-plusBtn.onclick = function( ) {
-    action = `+`
-    
-    if(input.value!=="") {
-        sum = Number(input.value) + sum
-    }
-    console.log(input.value,"перед сброса велиу")
-    input.value = ""
-    console.log(sum,"Первый цифр")
-    console.log(input.value,"после сброса велиу")
-    }
+multiplicationBtn.onclick = function () {
+  sum = 1;
+  if (input.value !== '') {
+    sum = Number(input.value) * sum;
+  }
+  input.value = '';
+  action = '*';
+  countSubmit = 0;
+};
 
-    
-submitBtn.onclick = function( ) {
-    submitcount = submitcount + 1
-    console.log(submitcount)
-}
-
-
-minusBtn.onclick = function( ) {
-    action = `-`
-}
-
-multiplicationBtn.onclick = function( ) {
-    action = `*`
-    
-}
-
-divisionBtn.onclick = function( ) {
-    action = `/`
-}
-
-// submitBtn.onclick = function () {
-
-//     if (action == `+`) {
-//         const sum = Number(input1.value) + Number(input2.value)
-//         input1.value = sum
-//     }
-//     if (action == `-`) {
-//         const sum = Number(input1.value) - Number(input2.value)
-//         resultElement.textContent = sum
-//     }
-//     if (action == `*`) {
-//         const sum = Number(input1.value) * Number(input2.value)
-//         resultElement.textContent = sum
-//     }
-//     if (action == `/`) {
-//         const sum = Number(input1.value) / Number(input2.value)
-//         resultElement.textContent = sum
-//     }
-// }
+divisionBtn.onclick = function () {
+  sum = 1;
+  if (input.value !== '') {
+    sum = Number(input.value) / sum;
+  }
+  input.value = '';
+  action = '/';
+  countSubmit = 0;
+};
